331039c356bc7a8dc604d0e9e82faa0b265d0a76
Marina <email.for.marina@gmail.com>
2022-09-05 23:43:46 +0400

    temporary solution for DishOrder

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,47 +1,46 @@
 package com.epam.collections.queue;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
         List<Integer> response = new LinkedList<>();
-        ArrayDeque<Integer> queue = new ArrayDeque<>(); //11 блюд
+        Queue<Integer> queue = new LinkedList<>();
+        LinkedList<Integer> temp = new LinkedList<>();
+
+
         for (int x = 1; x <= numberOfDishes; x++) {
             queue.offer(x);
         }
 
-
- for (int x = 0; x < queue.size(); x++) {
-     Integer first = queue.poll();
-     Integer second = queue.poll();
-     Integer third = queue.poll();
-
-     if (first != null && second != null && third != null) {
-         response.add(third);
-     } else if (third == null) {
-         queue.addFirst(second);
-         queue.addFirst(first);
-     } else if (second == null) {
-         queue.addFirst(first);
-     } else if (queue.size() == 2) {
-         response.add(queue.pollLast());
-         response.add(queue.pollLast());
-     }
+        while (queue.size() >= everyDishNumberToEat) {
+            int count = 1;
+            for (int y = 1; y <= everyDishNumberToEat; y++) {
+                Integer n = queue.poll();
+                if (n != null) {
+                    if (count == everyDishNumberToEat) {
+                        response.add(n);
+                        count = 1;
+                    } else {
+                        temp.add(n);
+                        count++;
+                    }
+                }
+            }
+            temp.addAll(0, queue);
+            if (temp.size() == 3) {
+                response.add(temp.pollFirst());
+                response.add(temp.pollLast());
+                response.add(temp.poll());
+            }
+            queue.clear();
+            queue.addAll(temp);
+            temp.clear();
         }
-
-
-
-
-
-
         return response;
-    }//4, 8, 1, 6, 11, 7, 3, 2, 5, 10, 9
-
-    public static void main(String[] args) {
-        DishOrderDeterminer a = new DishOrderDeterminer();
-        List<Integer> res = a.determineDishOrder(11, 4);
-        System.out.println(res);
-
     }
 }
 
